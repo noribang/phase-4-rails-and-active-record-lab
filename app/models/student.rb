0e86b0b4ed7d1584a2b762d2f3c72767370be3ca
hoonkim <hoonk9387@gmail.com>
2022-08-18 22:32:58 -0500

    Completed assignment. Ran migration and added instance method.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+
+    # return the student's first name 
+    # and last name concatenated with a space between
+    def to_s
+        "#{self.first_name} #{self.last_name}"
+    end
 end
